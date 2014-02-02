4bd3439dc5c12d6fe1aff28cb40c0db065836f50
michael mccune <msm@opbstudios.com>
2014-02-02 12:42:55 -0500

    adding a test for RunSeries and cleaning up from error return refactor

--- tabletesting_test.go
+++ tabletesting_test.go
@@ -4,26 +4,40 @@ import (
     "testing"
 )
 
+func TestRunSeries(t *testing.T) {
+    testers := []TableTester{
+        StringTableTester{"Field1", "test value"},
+        StringTableTester{"Field2", "another test value"},
+    }
+    testee := struct{
+            Field1 string
+            Field2 string
+        }{"test value", "another test value"}
+    if err := RunSeries([]TableTester(testers), testee); err != nil {
+        t.Errorf("RunSeries failed with error \"%s\"\n", err)
+    }
+}
+
 func TestStringTableTesterRunAgainst(t *testing.T) {
     tester := StringTableTester{ "Field1", "test value"}
     testee := struct{Field1 string}{"test value"}
-    if tester.RunAgainst(testee, t) == false {
-        t.Error("RunAgainst returned false, expected true\n")
+    if err := tester.RunAgainst(testee); err != nil {
+        t.Errorf("RunAgainst failed with error \"%s\"\n", err)
     }
 }
 
 func TestStringTableTesterRunAgainstFieldFailure(t *testing.T) {
     tester := StringTableTester{ "Field1", "test value"}
     testee := struct{Field2 string}{"test value"}
-    if tester.RunAgainst(testee, t) == true {
-        t.Error("RunAgainst returned true, expected false\n")
+    if err := tester.RunAgainst(testee); err == nil {
+        t.Error("RunAgainst produced no error when given a mismatched field entry\n")
     }
 }
 
 func TestStringTableTesterRunAgainstValueFailure(t *testing.T) {
     tester := StringTableTester{ "Field1", "test value"}
     testee := struct{Field1 string}{"bad test value"}
-    if tester.RunAgainst(testee, t) == true {
-        t.Error("RunAgainst returned true, expected false\n")
+    if err := tester.RunAgainst(testee); err == nil {
+        t.Error("RunAgainst produced no error when given a mismatched value entry\n")
     }
 }
