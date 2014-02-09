a0a143998f380404eefbed5717341c087e641d35
michael mccune <msm@opbstudios.com>
2014-02-08 22:55:35 -0500

    adding a uint table tester

--- tabletesting.go
+++ tabletesting.go
@@ -45,3 +45,27 @@ func (stt StringTableTester) RunAgainst(ti interface{}) error {
     }
     return nil
 }
+
+// A TableTester for uint based data.
+type UintTableTester struct {
+    Field string
+    Value uint
+}
+
+// Run the TableTester against the supplied data.
+// Looks for a field in the supplied data name Field and compares it's value
+// to Value, returns error or nil.
+func (utt UintTableTester) RunAgainst(ti interface{}) error {
+    field := reflect.ValueOf(ti).FieldByName(utt.Field)
+    if field.IsValid() == false {
+        err := fmt.Errorf("Failed to find field \"%s\"\n", utt.Field)
+        return err
+    }
+    actual := field.Interface()
+    expected := utt.Value
+    if actual != expected {
+        err := fmt.Errorf("Fail on %s, expected \"%d\", actual \"%d\"\n", utt.Field, expected, actual)
+        return err
+    }
+    return nil
+}
